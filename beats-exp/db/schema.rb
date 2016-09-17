38eae36cee9240e188eec6a17e0bcf18fd00b301
Amine Belkhiria <amine@videri-concept.de>
2016-09-17 14:07:06 +0200

    Backend Graphs

--- beats-exp/db/schema.rb
+++ beats-exp/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160913152252) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ravers", force: :cascade do |t|
     t.string   "name"
     t.string   "surname"
