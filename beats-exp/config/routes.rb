54b773729e8e29b4b4b832d5a00c83f188a688dc
Amine Belkhiria <amine@videri-concept.de>
2016-09-17 23:45:03 +0200

    charts and expenses backend

--- beats-exp/config/routes.rb
+++ beats-exp/config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   namespace :backend do
     resources :ravers
+    get "expenses", to: "expenses#index"
    end
   
   # Example of regular route:
